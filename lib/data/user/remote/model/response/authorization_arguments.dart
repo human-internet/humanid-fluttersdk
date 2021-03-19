4562dadc091fc2bb1141fe71bd9a9cc5768ba563
Fiorent Nicholas Yehardi <fionicholas05@gmail.com>
2021-03-19 19:15:01 +0700

    style(authorization) : update logic authorization

--- lib/data/user/remote/model/response/authorization_arguments.dart
+++ lib/data/user/remote/model/response/authorization_arguments.dart
@@ -3,6 +3,14 @@ class AuthorizationArguments {
   final String iconUrl;
   final String clientId;
   final String clientSecret;
+  final String dialCode;
+  final String phoneNumber;
 
-  AuthorizationArguments({this.appName, this.iconUrl, this.clientId, this.clientSecret});
+  AuthorizationArguments(
+      {this.appName,
+      this.iconUrl,
+      this.clientId,
+      this.clientSecret,
+      this.dialCode,
+      this.phoneNumber});
 }
